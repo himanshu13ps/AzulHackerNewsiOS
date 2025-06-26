0382b08ecce4e7228c65ac853485c101fdd58d4f
Himanshu Shringarpure <shringah@amazon.com>
2025-06-26 11:36:57 +0530

    Initial Commit

---
+++ AzulHackerNews/AzulHackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AzulHackerNewsApp.swift
+//  AzulHackerNews
+//
+//  Created by Shringarpure, Himanshu on 26/06/25.
+//
+
+import SwiftUI
+
+@main
+struct AzulHackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AzulHackerNewsTests/AzulHackerNewsTests.swift
@@ -0,0 +1,17 @@
+//
+//  AzulHackerNewsTests.swift
+//  AzulHackerNewsTests
+//
+//  Created by Shringarpure, Himanshu on 26/06/25.
+//
+
+import Testing
+@testable import AzulHackerNews
+
+struct AzulHackerNewsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
